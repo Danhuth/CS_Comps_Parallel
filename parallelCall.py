ed9ba176fa2a11fbc5c01277994a78294d258411
Daniel Huth <dhuth@oxy.edu>
2019-11-21 23:45:02 -0800

    adjusted minor

--- parallelCall.py
+++ parallelCall.py
@@ -4,13 +4,15 @@ from clusterun import sequencerun
 import os
 import re
 
-def Output_Collator(filePath):
-    boundruntimestring = ''
-    count = 0
-    finalOutput = open("Collated_Output.txt",'w+')
-    with open(filePath) as f:
-        data = f.read()
-        match = re.findall('gap>\s+(\d+)\s+gap>\s+(\d+)', data)
-    for i in range(len(match)):
-        finalOutput.write(str(match[i])+'\n')
+def pathtuplecreator():
+    generator_set_list = []
+    #construct list of generator set folders.
+    #Now we have the top level _folders_  
+    #parallelgapcall handles digging the files out of these directories
+    for i in range(0,10): 
+        generator_storage_i = str(os.getcwd())+"/Generator_Storage_Folder"+str(i)
+        generator_set_list.append(generator_storage_i)
+
+    return(generator_set_list)
+pathTuple = (pathtuplecreator(),os.getcwd()+"/outputfile")
 sequencerun(GapCaller(pathTuple),pathTuple)
