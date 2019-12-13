fc743e07be62bbe121d3addc644570e0fe5010a2
Daniel Huth <dhuth@oxy.edu>
2019-12-13 11:29:13 -0800

    final commit for parallel program

--- Output_Collator.py
+++ Output_Collator.py
@@ -1,13 +1,17 @@
 import os
 import re
+import glob
 
 def Output_Collator(filePath):
     boundruntimestring = ''
     count = 0
-    finalOutput = open("Collated_Output.txt",'w+')
+    finalOutput = open("Collated_Output.txt",'a')
     with open(filePath) as f:
         data = f.read()
-        match = re.findall('gap>\s+(\d+)\s+gap>\s+(\d+)', data)
-        print(match)
+        #print(data)
+        match = re.findall('brk>\s+(\d+)\s+brk>\s+(\d+)', data)
+        #print(match)
     for i in range(len(match)):
         finalOutput.write(str(match[i])+'\n')
+for name in glob.glob(os.getcwd()+'/clusterun*'):
+    Output_Collator(name)
